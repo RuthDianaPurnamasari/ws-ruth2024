3d42ded31455ec82a1b59a79be7533c11106ef03
RuthDianaPurnamasari <ruthdianaps@gmail.com>
2024-06-14 15:04:02 +0700

    bismillah

--- main.go
+++ main.go
@@ -12,7 +12,21 @@ import (
 	"github.com/RuthDianaPurnamasari/ws-ruth2024/url"
 
 	"github.com/gofiber/fiber/v2"
+	_ "github.com/RuthDianaPurnamasari/ws-ruth2024/docs"
+
 )
+
+// @title TES SWAGGER ULBI
+// @version 1.0
+// @description This is a sample swagger for Fiber
+
+// @contact.name API Support
+// @contact.url https://github.com/RuthDianaPurnamasari
+// @contact.email 714220042@std.ulbi.ac.id
+
+// @host ws-ruth2024-458a1e70d2e1.herokuapp.com
+// @BasePath /
+// @schemes https http
 
 func main() {
 	site := fiber.New(config.Iteung)
